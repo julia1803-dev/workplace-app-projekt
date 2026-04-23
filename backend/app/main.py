cbe5de5581dfc5cbaff8a887cd7eb83c3b8f1e08
u236358 <yuliya.krause2@sbb.ch>
2026-04-23 10:44:50 +0200

    Add comments and explanation to main application setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.database import create_db
-from app.routers import router
-from app.seed_data import seed_data
+from fastapi import FastAPI #erstellt deine API
+from fastapi.middleware.cors import CORSMiddleware #erlaubt Zugriff vom Frontend
+from app.database import create_db #erstellt Tabellen
+from app.routers import router #enthält alle API-Endpunkte
+from app.seed_data import seed_data #fügt Testdaten ein
 
-app = FastAPI(title="Workplace Booking App")
+app = FastAPI(title="Workplace Booking App") #Initialisierung FastAPI-Anwendung
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,11 +15,15 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) #frontend und backend läuft separat, die Verbindung wird nicht blockiert
 
 @app.on_event("startup")
 def on_startup():
-    create_db()
-    seed_data()
+    create_db() #Tabellen Erstellung
+    seed_data() #Einfügen Beispieldaten
 
-app.include_router(router)
+@app.get("/")
+def root():
+    return {"message": "API is running"}
+
+app.include_router(router) #Router einbinden
